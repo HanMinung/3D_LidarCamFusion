68b5283d3934c2f9b2301b840ec24b0d05cccda0
HanMinung <99113269+HanMinung@users.noreply.github.com>
2023-08-04 19:33:48 +0900

    delivery mission code updated

--- src_file/serial_node.py
+++ src_file/serial_node.py
@@ -11,36 +11,37 @@ class erpSerial :
         self.ser = serial.Serial(device_name, baudrate=115200)
         self.ser.flushInput()
         self.ser.flushOutput()
-        # thread = threading.Thread(target=self.recv_serial_data)
-        # thread.daemon = True 
-        # thread.start() 
+        thread = threading.Thread(target=self.recv_serial_data)
+        thread.daemon = True 
+        thread.start() 
         self.alive=0
         self.is_data=False
         self.speed=0.0
         self.steer=0.0
 
-    # def recv_serial_data(self):
-    #     while True :
-    #         packet = self.ser.readline()
-    #         if len(packet)==18:
-    #             header= packet[0:3].decode()
+    def recv_serial_data(self):
+        while True :
+            packet = self.ser.readline()
+            if len(packet)==18:
+                header= packet[0:3].decode()
                 
-    #             if header == "STX":
-    #                 self.is_data=True
-    #                 mode,e_stop,gear=struct.unpack('BBB',packet[3:6])
-    #                 self.speed,self.steer=struct.unpack('2h',packet[6:10])
-    #                 brake=struct.unpack('B',packet[10:11])
-    #                 enc=struct.unpack('f',packet[11:15])
-    #                 alive=struct.unpack('B',packet[15:16])
-    #                 # print(speed,steer)
+                if header == "STX":
+                    self.is_data=True
+                    mode,e_stop,gear=struct.unpack('BBB',packet[3:6])
+                    self.speed,self.steer=struct.unpack('2h',packet[6:10])
+                    brake=struct.unpack('B',packet[10:11])
+                    enc=struct.unpack('f',packet[11:15])
+                    alive=struct.unpack('B',packet[15:16])
+                    # print(speed,steer)
 
-    def send_ctrl_cmd(self,velocity,steering, braking):
+
+    def send_ctrl_cmd(self,velocity,steering, brake):
         header="STX".encode()
         tail='\r\n'.encode()
         mode=1
         e_stop=0
         gear=0
-        brake=braking
+        brake=1
      
         data=struct.pack('>BBBhhBB',mode,e_stop,gear,velocity*10,steering*71,brake,self.alive)
         packet=header+data+tail
